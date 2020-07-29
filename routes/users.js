3292cf7ac307b279eb5caa3f92cb2eb7ea4666d0
alimam15 <46492827+alimam15@users.noreply.github.com>
2020-07-29 15:37:27 -0400

    adding Patch

--- routes/users.js
+++ routes/users.js
@@ -48,6 +48,21 @@ router.delete('/:id', (req,res) => {
     res.send(`User with id :${id} was deleted`);
 
 })
+
+
+router.patch("/:id", (req, res) => {
+
+    const {id} = req.params;
+    const {firstName, lastName, age} = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if(firstName) user.firstName = firstName;
+    if(lastName) user.lastName = lastName;
+    if(age) user.age = age;
+
+    res.send(`The user with the id ${id} has been updated`);
+})
     
 
 export default router;
